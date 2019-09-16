e31ae40ff3155557433f6c08accc277b131ca4a6
vanesnery <nery_van@hotmail.com>
2019-09-16 17:59:19 -0300

    inserindo filtro

--- src/data.js
+++ src/data.js
@@ -1,24 +1,33 @@
 //FUNÇOES AQUIIIIII
+const arrayPokemons = POKEMON.pokemon;
+
+window.data ={
+  listPokemon: listPokemon,
+  };
 
 document.body.onload = listPokemon;
 
-function listPokemon () { 
+function listPokemon (pokemons) { 
   // cria um novo elemento div e dá à ele conteúdo
- const pokemons = POKEMON.pokemon
-
   for(const i of pokemons){
     const divNova = document.createElement("div");
-    divNova.setAttribute("class", "divmain")
+    divNova.setAttribute("class", "divmain");
     const name = document.createTextNode("\n"+i.name); 
-    const id = document.createTextNode("\n"+i.id);
+    const num = document.createTextNode("\n"+i.num);
     const img = document.createElement('img');
     img.src = i.img;
+
     //adiciona o nó de texto à nova div criada 
-    divNova.appendChild(id); 
+    divNova.appendChild(num); 
     divNova.appendChild(name);
     divNova.appendChild(img);
-    
+            
     // adiciona o novo elemento criado e seu conteúdo ao DOM 
     const divAtual = document.getElementById("div1"); 
     document.body.insertBefore (divNova, divAtual); 
   }
+}
+
+function listTypes (pokemons, type) {
+ return pokemons.filter(poke => poke.type.includes(type));
+}
