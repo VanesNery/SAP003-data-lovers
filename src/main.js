6c9c4b7ffb07740b6bb889fcf47062b7f079492a
vanesnery <nery_van@hotmail.com>
2019-09-18 13:21:30 -0300

    mudando a funçao de mostrar os pokemons

--- src/main.js
+++ src/main.js
@@ -3,31 +3,37 @@ const pokemons = POKEMON.pokemon;
 const filtro = document.getElementById("optionFilter");
 const show = document.getElementById("show");
 
-// window.onload = listPokemon => {
-//     event.preventDefault();
-//     const divAtual = document.getElementById("div1"); 
-//     const divNova = document.getElementById("divmain");
-//     const listP = window.data.listPokemon();
-// } 
-
-filtro.addEventListener("change", lTypes);
-
-window.onload = () =>{
-    lTypes (pokemons);
+window.onload = () => {
+    listPoke ();
 }
 
-function lTypes () { 
-const pTypes = [];
-pokemons.map(pokemons => pokemons.type.map(type => {
-    if(!pTypes.includes(type)){
-        pTypes.push(type);
-    }else{
-            return false;
-        }
-    }
-));
+function listPoke (){
+pokemons.map(pokemon => show.innerHTML += `
+    <div id="card" class="card">
+    <span>nº${pokemon.num}</span>
+    <h2>${pokemon.name}</h2>
+    <img src="${pokemon.img}"/>
+    </div>
+`);
+}
+// const filter = app.listTypes(pokemons, lTypes.value);
 
-// filtro.innerHTML ="";
+// filtro.addEventListener("change", listTypes);
+
+// window.onload = () => {
+//     lTypes ();
+// }
+
+// function lTypes () { 
+// const pTypes = [];
+// pokemons.map(pokemons => pokemons.type.map(type => {
+//     if(!pTypes.includes(type)){
+//         pTypes.push(type);
+//     }else{
+//             return false;
+//     }
+//     }
+// ));
 // filtro.innerHTML = `<option value="name">-- Filtro --</option>`;
-// filtro.innerHTML += pTypes.map(`<option value="${type}">${type}</option>`).join("");
-}
+// filtro.innerHTML += (`<option value="${lTypes}">${lTypes}</option>`).join("");
+// }
