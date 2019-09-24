41530591170b63feb173ffea1befd6654541d7dc
vanesnery <nery_van@hotmail.com>
2019-09-24 02:47:01 -0300

    ordenando com sucesso

--- src/data.js
+++ src/data.js
@@ -8,7 +8,20 @@ function listText(pokemons, text) {
   return pokemons.filter(poke => poke.name.includes(text));
 }
 
+function listOrder(pokemons, Orde) {
+  if (Orde === "A-Z") {
+    return pokemons.sort((a, b) => (a.name > b.name?1:-1));
+  }
+  else if (Orde === "Z-A") {
+    return pokemons.sort((a, b) => (a.name > b.name?-1:1));
+  }
+  else {
+    return pokemons;
+  }
+}
+
 app = {
   listTypes,
-  listText
+  listText,
+  listOrder
 };
--- test/data.spec.js
+++ test/data.spec.js
@@ -12,11 +12,20 @@ describe("FilterPoke", () => {
   });
 });
 
+describe("FilterAZ", () => {
+  it("should be a function", () => {
+    expect(typeof app.listOrder).toBe("function");
+  });
+});
+
 it("returns `example`", () => {
   expect(app.listTypes([{"type": ["Grass", "Poison"]}], "Grass")).toEqual([{"type": ["Grass", "Poison"]}], "Grass");
 });
 
 it("returns `example`", () => {
-  expect(app.listText([{"name": "Bulbasaur", "num": "001", "type": ["Grass", "Poison"]}], "Bulbasaur")).toEqual([{"name": "Bulbasaur", "num": "001", "type": ["Grass", "Poison"]}], "Bulbasaur");
+  expect(app.listText([{"name": "Bulbasaur", "num": "001", "type": ["Grass", "Poison"]}], "Bulbasaur")).toEqual([{"name": "Bulbasaur", "num": "001", "type": ["Grass", "Poison"]}]);
 });
-  
+
+it("returns `example`", () => {
+  expect(app.listOrder([{"num": "001", "name": "Bulbasaur", "type": ["Grass", "Poison"]}, {"num": "002", "name": "Ivysaur", "type": ["Grass", "Poison"], }, {"num": "003", "name": "Venusaur", "type": ["Grass", "Poison"]}], "Z-A")).toEqual([{"num": "003", "name": "Venusaur", "type": ["Grass", "Poison"]}, {"num": "002", "name": "Ivysaur", "type": ["Grass", "Poison"]}, {"num": "001", "name": "Bulbasaur", "type": ["Grass", "Poison"]}]);
+});
